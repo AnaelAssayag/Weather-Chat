d19036f0ec8dfd2a51cb71d74110ad4d330e7ca8
Anael Dahan <anael.assayag@gmail.com>
2018-01-08 13:11:26 +0200

    test to resolve bug

--- public/script.js
+++ public/script.js
@@ -1,6 +1,3 @@
-
-debugger
-
 var fetch = function () {
     $.ajax({
         method: "GET",
@@ -9,14 +6,14 @@ var fetch = function () {
         success: function (data) {
             console.log(data);
             console.log(data.main.temp)
-            var array = [{name: "Marseille", temp: 18, tempF: 48, hour:10, date:'01/01/2018'}]
-            var dataArray = {data: array};
+            // var array = [{name: "Marseille", temp: 18, tempF: 48, hour:10, date:'01/01/2018'}]
+            // var dataArray = {data: array};
             renderArray();
 
 
 
             
-            displayWeather(data);
+            // displayWeather(data);
 
         },
         error: function (jqXHR, textStatus, errorThrown) {
@@ -27,112 +24,36 @@ var fetch = function () {
 
 $('.searchF').on('click', fetch)
 
-
+var data_array = { 
+    data:[
+      {id:1,
+      name: 'Marseille', 
+      temp: 18, 
+      tempF: 48,
+      hour:10, 
+      date:'01/01/2018',
+      comment_array:['good', 'I love it']
+   },
+     {
+     id:2,
+     name: 'Paris', 
+     temp: 0, 
+     tempF: 48,
+     hour:10, 
+     date:'01/01/2018',
+     comment_array:['cold here']
+     }]
+    }
 
 
 var renderArray = function () {
     //Set date in specific format
-    var today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth() + 1;
-    var yyyy = today.getFullYear();
-    if (dd < 10) {
-        dd = '0' + dd;
-    }
-
-    if (mm < 10) {
-        mm = '0' + mm;
-    }
-    today = dd + '/' + mm + '/' + yyyy;
     var source = $('#store-template').html();
     var template = Handlebars.compile(source);
-    var newHTML = template({data:array}
+    var newHTML = template(data_array)
 
-                // hour: today.getHours()
-);
+    $('.weatherT').append(newHTML)
+
+                // hour: today.getHours(
 
 }
-
-
-
-
-// var pushTemp = function(city, temp) {
-//     var array = [];
-//     pushArray({city:city, temp:temp}) 
-// }
-
-// var findTemp = function () {
-//     var cityName = $(this).closest('div').find('input');
-//     console.log(cityName)
-//     var cityTemp = data.main.temp
-//     console.log(cityTemp)
-// }
-
-// var renderTemp = function() {
-
-// }
-
-
-
-// function displayWeather(data) {
-//     //Set date in specific format
-//     var today = new Date();
-//     var dd = today.getDate();
-//     var mm = today.getMonth() + 1;
-//     var yyyy = today.getFullYear();
-//     if (dd < 10) {
-//         dd = '0' + dd;
-//     }
-
-//     if (mm < 10) {
-//         mm = '0' + mm;
-//     }
-//     today = dd + '/' + mm + '/' + yyyy;
-//     //Get city, temp & date in good format
-//     var source = $('#store-template').html();
-//     var template = Handlebars.compile(source);
-//     var newHTML = template({
-//         name: data.name.toUpperCase(),
-//         temp: Math.floor(data.main.temp - 273.15),
-//         tempF: Math.floor((data.main.temp * 9 / 5) - 459.67),
-//         date: today
-//         // hour: today.getHours()
-
-//     });
-//     $('.weatherT').append(newHTML);
-
-
-//     $('.commentButton').on('click', addComment)
-
-// }
-
-// //Add comment
-
-// var array = [];
-// //push data to the array
-// var updateArray = function (text) {
-//     array.push({ text: text })
-// }
-// // display data
-// var renderArray = function () {
-// // localStorage.getItem('text')
-//     $('.inputU').find('p').remove();
-//     for (var i = 0; i < array.length; i++) {
-//         var displayComment = $('<p>' + array[i].text + '<p>')
-//         console.log(displayComment)
-//         $('.inputU').append(displayComment)
-//     }
-
-// }
-// function addComment() {
-//     var uText = $(this).closest("div").find("input").val();
-//     console.log(uText)
-
-//     updateArray(uText);
-//     renderArray();
-
-// }
-
-
-
-
