b4b0caecafc4690a021ce01a503c9ffb10f9738d
Anael Dahan <anael.assayag@gmail.com>
2018-01-13 20:05:37 +0200

    resolved bug: add possibility to comment and remove cities

--- public/script.js
+++ public/script.js
@@ -4,12 +4,9 @@ var fetch = function () {
         url: "http://api.openweathermap.org/data/2.5/weather?q=" + $('#input_city').val()
             + "&appid=c8bdaccb89a38ddeae0eae1865d5dee7",
         success: function (data) {
-            console.log(data);
-            console.log(data.main.temp)
-            // var array = [{name: "Marseille", temp: 18, tempF: 48, hour:10, date:'01/01/2018'}]
-            // var dataArray = {data: array};
+            idnum = number++
             var obj = {
-                id: 12,
+                id: idnum,
                 name: data.name,
                 temp: Math.floor(data.main.temp - 273.15),
                 tempF: Math.floor((data.main.temp * 9 / 5) - 459.67),
@@ -17,19 +14,9 @@ var fetch = function () {
                 date: new Date(),
                 comment_array: []
             }
-            data_array['data'].push(obj)
 
-
+            weatherData['citiesArray'].push(obj)
             renderArray();
-
-
-
-
-
-
-            // displayWeather(data);
-
-            // set date to good format
 
         },
         error: function (jqXHR, textStatus, errorThrown) {
@@ -39,36 +26,42 @@ var fetch = function () {
 };
 
 $('.searchF').on('click', fetch)
-
-var data_array = {
-    data: []
+var number = 1
+var weatherData = {
+    citiesArray: []
 }
+// var number = 1
 
 
 var renderArray = function () {
-
-
     $('.weatherT').find('.template').remove();
     var source = $('#store-template').html();
     var template = Handlebars.compile(source);
-    var newHTML = template(data_array)
-
+    var newHTML = template(weatherData)
     $('.weatherT').append(newHTML)
-
     $('.searchC').on('click', addComment)
-
-    // hour: today.getHours(
-    // $('.fa-trash-o').on('click', remove)
-
-    // $('.searchC').on('click', addComment)
-
+    $('.fa-trash-o').on('click', removeCity)
 }
 
-var addComment = function() {
-    
+var addComment = function () {
+    var comm = $(this).siblings('.commentB').val();
+    var idCity = $(this).closest('.dataTemp').data().id;
+    for (var i = 0; i < weatherData.citiesArray.length; i++) {
+        if (weatherData.citiesArray[i].id == idCity)  {
+            weatherData.citiesArray[i].comment_array.push({ text: comm })
+        }
+    }
+    renderArray();
 }
 
-// var remove = function() {
-//     var elmt = $(this).closest('.dataTemp')
-//     array.filter
-// }
+var removeCity = function () {
+    var idCity = $(this).closest('.dataTemp').data().id;
+    console.log(idCity)
+
+    for (var i = 0; i < weatherData.citiesArray.length; i++) {
+        if (weatherData.citiesArray[i].id == idCity) {
+            weatherData.citiesArray.splice([i], 1)
+        }
+    }
+    renderArray();
+}
